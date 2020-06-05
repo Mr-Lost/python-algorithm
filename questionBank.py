92e9280311038a6266d936089f27ceb68f1dec26
Lambert <1624556674@qq.com>
2020-06-05 09:52:07 +0800

    first commit

--- questionBank.py
+++ questionBank.py
@@ -8,6 +8,15 @@ class Solution:
             else:
                 hashmap[val] = i
 
+    # 2.反转输出一个带符号的32位整数，其范围为[-2**31, 2**31 - 1]，若溢出则返回0
+    def reverse(self, num):
+        flag = 1
+        if num < 0:
+            flag = - flag
+            num = - num
+        temp = int(str(num)[::-1]) * flag
+        return temp if -2 ** 31 - 1 < temp < 2 ** 31 else 0
+
 
 if __name__ == '__main__':
     print(Solution().twoSum(nums=[0, 2, 4, 3], target=5))
