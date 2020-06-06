015b4fc1f2b3a483f774762fd1e2e4df38d19601
Lambert <1624556674@qq.com>
2020-06-06 23:13:28 +0800

    first commit

--- questionBank.py
+++ questionBank.py
@@ -17,6 +17,27 @@ class Solution:
         temp = int(str(num)[::-1]) * flag
         return temp if -2 ** 31 - 1 < temp < 2 ** 31 else 0
 
+    # 3.判断一个整数是否为回文数，从左读和从右读是同一个数(>=0)
+    def isPalindrome(self, num):
+        return str(num)[::-1] == str(num)
+        # (1)不转换为字符串
+        # ans = 0
+        # temp = num
+        # if num == 0:
+        #     return 1 == 1
+        # if num < 0 or num % 10 == 0:
+        #     return 1 == 0
+        # else:
+        #     while num > 0:
+        #         ans *= 10
+        #         ans += num % 10
+        #         num //= 10
+        #     if ans == temp:
+        #         return 1 == 1
+        #     else:
+        #         return 1 == 0
+
 
 if __name__ == '__main__':
     print(Solution().twoSum(nums=[0, 2, 4, 3], target=5))
+    print(Solution().isPalindrome(1234321))
