6c7716deaccd0174e1752a413eb5dc6ea960b2eb
Lambert <1624556674@qq.com>
2020-06-07 23:05:40 +0800

    first commit

--- questionBank.py
+++ questionBank.py
@@ -37,7 +37,33 @@ class Solution:
         #     else:
         #         return 1 == 0
 
+    # 4.罗马数字转整数(IV=4, IX=9, XL=40, XC=90, CD=400, CM=900)
+    def romanToInt(self, roman):
+        temp = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        ans = 0
+        for i in range(len(roman)):
+            if i < len(roman)-1 and temp.get(roman[i]) < temp.get(roman[i+1]):
+                ans -= temp.get(roman[i])
+            else:
+                ans += temp.get(roman[i])
+        return ans
+        # (1)这样定义双字母值，便于下面循环操作中值的叠加
+        # temp = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+        #         'IV': 3, 'IX': 8, 'XL': 30, 'XC': 80, 'CD': 300, 'CM': 800}
+        # ans = 0
+        # for i in range(len(roman)):
+        #     if roman[i-1:i+1] in temp:   # roman[-1:1]=''
+        #         ans += temp.get(roman[i-1:i+1])
+        #     else:
+        #         ans += temp.get(roman[i])
+        # return ans
+        # (2)
+        # temp = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000,
+        #         'IV': 3, 'IX': 8, 'XL': 30, 'XC': 80, 'CD': 300, 'CM': 800}
+        # return sum(temp.get(roman[i-1:i+1], temp[n]) for i, n in enumerate(roman))
+
 
 if __name__ == '__main__':
     print(Solution().twoSum(nums=[0, 2, 4, 3], target=5))
     print(Solution().isPalindrome(1234321))
+    print(Solution().romanToInt('MCMXCIV'))
