0d3a7416e1b5ce3df4da67a9f6b14f58e0dda8d1
Lambert <1624556674@qq.com>
2020-06-10 23:49:57 +0800

    tbc

--- questionBank.py
+++ questionBank.py
@@ -83,6 +83,13 @@ class Solution:
                 return False
         return len(stack) == 1
 
+    # 7.删除一个排序数列中重复的元素后，返回新数组的长度
+    def removeDuplicates(self, nums):
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i] == nums[i-1]:
+                nums.pop(i)
+        return len(nums)
+
 
 if __name__ == '__main__':
     lmr = Solution()
