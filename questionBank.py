c91c7d54e31a340de61f24e1978d242c1d80f0c1
Lambert <1624556674@qq.com>
2020-06-22 23:37:35 +0800

    tbc

--- questionBank.py
+++ questionBank.py
@@ -6,7 +6,7 @@ class ListNode:
 
 class Solution:
     # #easy
-    # 1.数组中找出和为给定值的一组数并返回其下标
+    # 1.两数之和：数组中找出和为给定值的一组数并返回其下标
     def twoSum(self, nums, target):
         hashmap = dict()
         for i, val in enumerate(nums):
@@ -15,7 +15,7 @@ class Solution:
             else:
                 hashmap[val] = i
 
-    # 7.反转输出一个带符号的32位整数，其范围为[-2**31, 2**31 - 1]，若溢出则返回0
+    # 7.整数反转：反转输出一个带符号的32位整数，其范围为[-2**31, 2**31 - 1]，若溢出则返回0
     def reverse(self, num):
         flag = 1
         if num < 0:
@@ -24,7 +24,7 @@ class Solution:
         temp = int(str(num)[::-1]) * flag
         return temp if -2 ** 31 - 1 < temp < 2 ** 31 else 0
 
-    # 9.判断一个整数是否为回文数，从左读和从右读是同一个数(>=0)
+    # 9.回文数：判断一个整数是否为回文数，从左读和从右读是同一个数(>=0)
     def isPalindrome(self, num):
         return str(num)[::-1] == str(num)
         # (1)不转换为字符串
@@ -69,7 +69,7 @@ class Solution:
         #         'IV': 3, 'IX': 8, 'XL': 30, 'XC': 80, 'CD': 300, 'CM': 800}
         # return sum(temp.get(roman[i-1:i+1], temp[n]) for i, n in enumerate(roman))
 
-    # 14.查找字符串数组中最长公共前缀
+    # 14.最长公共前缀：查找字符串数组中最长公共前缀
     def longestCommonPrefix(self, strs):
         ans = ''
         for i in zip(*strs):
@@ -79,7 +79,7 @@ class Solution:
                 break
         return ans
 
-    # 20.判断左右括号闭合是否有效
+    # 20.有效的括号：判断左右括号闭合是否有效
     def isValid(self, s):
         temp = {'(': ')', '[': ']', '{': '}', '/': '/'}
         stack = ['/']   # 赋初值为了避免没有元素时，pop()报错
@@ -90,7 +90,7 @@ class Solution:
                 return False
         return len(stack) == 1
 
-    # 26.删除一个排序数列中重复的元素后，返回新数组的长度
+    # 26.删除排序数组中的重复项：删除一个排序数列中重复的元素后，返回新数组的长度
     def removeDuplicates(self, nums):
         nums.sort()
         for i in range(len(nums)-1, 0, -1):
@@ -98,7 +98,7 @@ class Solution:
                 nums.pop(i)
         return len(nums)
 
-    # 27.删除数组中所有的某个特定值，并返回数组长度
+    # 27.移除元素：删除数组中所有的某个特定值，并返回数组长度
     def removeElement(self, nums, val):
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] == val:
@@ -119,7 +119,7 @@ class Solution:
         # (1)
         # return str1.find(str2) if str2 != '' else 0
 
-    # 35.在一个升序排列的数组中查找指定元素，有则返回下标，无则返回按序插入的位置
+    # 35.搜索插入位置：在一个升序排列的数组中查找指定元素，有则返回下标，无则返回按序插入的位置
     def searchInsert(self, nums, target):
         if target not in nums:
             nums.append(target)
@@ -134,7 +134,7 @@ class Solution:
         #     if target <= i:
         #         return nums.index(i)
 
-    # 38.外观数列，下一项描述上一项数值依次是a个x，b个y，c个z ... (xyyxzzz -> 1x2y1x3z)
+    # 38.外观数列：下一项描述上一项数值依次是a个x，b个y，c个z ... (xyyxzzz -> 1x2y1x3z)
     def countAndSay(self, n):
         exam = '1'
         for i in range(n - 1):
@@ -167,7 +167,7 @@ class Solution:
         # return ans
 
     # #normal
-    # 2.非空链表逆序存储两个正数，返回相加结果的新链表
+    # 2.两数相加：非空链表逆序存储两个正数，返回相加结果的新链表
     def addTwoNumbers(self, li1, li2):
         head = temp = ListNode(None)
         ans = 0
@@ -180,7 +180,7 @@ class Solution:
             li2 = li2.next if li2 else None
         return head.next
 
-    # 3.返回字符串中最长不重复子串的长度
+    # 3.无重复字符的最长子串：返回字符串中最长不重复子串的长度
     def lengthOfLongestSubstring(self, s):
         temp = dict()
         index, ans = 0, 0
@@ -191,18 +191,41 @@ class Solution:
             temp[s[i]] = i + 1
         return ans
 
-    # 4.最长回文子串
+    # 5.最长回文子串
     def longestPalindrome(self, s):
-        ans = ''
         if len(s) < 2:
             return s
+        ans = s[0]
         for i in range(len(s) - 1):
-            for j in range(i + 1, len(s)):
-                mid = (j + i) // 2
-                temp = s[mid + 1:j + 1] if len(s[i:j+1]) % 2 == 0 else s[mid:j + 1]
-                if s[i:mid + 1] == temp[::-1] and len(s[i:j + 1]) > len(ans):
-                    ans = s[i:j + 1]
+            for j in range(i, len(s)):
+                temp = s[i:j + 1]
+                if temp == temp[::-1] and len(temp) > len(ans):
+                    ans = temp
         return ans
+        # 超时
+        # ans = ''
+        # if len(s) < 2:
+        #     return s
+        # for i in range(len(s) - 1):
+        #     for j in range(i + 1, len(s)):
+        #         mid = (j + i) // 2
+        #         temp = s[mid + 1:j + 1] if len(s[i:j+1]) % 2 == 0 else s[mid:j + 1]
+        #         if s[i:mid + 1] == temp[::-1] and len(s[i:j + 1]) > len(ans):
+        #             ans = s[i:j + 1]
+        # return ans
+
+    # 6.Z字形变换：将字符串各个字符从上到下再往上，往返逐行各插入一个字符，最后输出按行拼接的结果
+    def convert(self, s, numRows):
+        if numRows < 2:
+            return s if numRows == 1 else ''
+        temp = [''] * numRows
+        row, flag = 0, 1
+        for i in s:
+            temp[row] += i
+            row += flag
+            if row == numRows - 1 or row == 0:
+                flag = -flag
+        return ''.join(temp)
 
 
 if __name__ == '__main__':
@@ -216,8 +239,8 @@ if __name__ == '__main__':
     # print(lmr.strStr('good', ''))
     # print(lmr.countAndSay(4))
     # print(lmr.lengthOfLongestSubstring('acgbsdseiasdg'))
-    print(lmr.longestPalindrome('babad'))
-
+    # print(lmr.longestPalindrome('bb'))
+    print(lmr.convert('LEETCODEISHIRING', 3) == 'LCIRETOESIIGEDHN')
 
 
 
