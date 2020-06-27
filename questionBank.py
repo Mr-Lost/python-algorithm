cf8b58e2671be4657a1fd1daeb5de9b6f19c43f6
Lambert <1624556674@qq.com>
2020-06-28 07:36:55 +0800

    tbc

--- questionBank.py
+++ questionBank.py
@@ -281,11 +281,13 @@ class Solution:
         nums.sort()
         ans = []
         for i in range(len(nums)-1):
-            if nums[i] >= 0:
+            if nums[i] > 0:
                 break
-            if nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i+1, len(nums)):
+                if j > i+1 and nums[j] == nums[j-1]:
+                    continue
                 if -(nums[i] + nums[j]) in nums[j+1:]:
                     ans.append([nums[i], nums[j], -(nums[i] + nums[j])])
         return ans
