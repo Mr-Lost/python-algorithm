6ae937a67aeff574754b9330a3d78feb8437e62c
Dawnrain <1624556674@qq.com>
2022-03-21 18:02:20 +0800

    tbc

--- nowcoderQuestions.py
+++ nowcoderQuestions.py
@@ -1,6 +1,6 @@
 class Nowcoder:
+    # HJ6质数因子：分解成所有质数相乘
     def all_prime(self, num):
-        # HJ6质数因子：分解成所有质数相乘
         is_prime = True
         for i in range(2, int(num**0.5)+1):
             if num % i == 0:
@@ -11,6 +11,48 @@ class Nowcoder:
                 break
         if is_prime:
             print(num)
+
+    # NC17最长回文子串
+    def longest_palindrome(self, word):
+        for i in range(len(word)):
+            for j in range(len(word)//2+1):
+                pass
+
+    # NC52有效括号序列
+    def valid_brackets(self, word):
+        stack = []
+        dic = {'(': ')', '[': ']', '{': '}'}
+        for i in word:
+            if i in dic:
+                stack.append(dic[i])
+            else:
+                if len(stack):
+                    if i == stack.pop():
+                        continue
+                return False
+        return not len(stack)
+
+    # NC109岛屿数量
+    def islands_num(self, grid):
+        def dfs(grid, i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
+                return
+            grid[i][j] = '0'
+            dfs(grid, i+1, j)
+            dfs(grid, i-1, j)
+            dfs(grid, i, j+1)
+            dfs(grid, i, j-1)
+
+        ans = 0
+        if len(grid):
+            if len(grid[0]):
+                m, n = len(grid), len(grid[0])
+                for x in range(m):
+                    for y in range(n):
+                        if grid[x][y] == '1':
+                            ans += 1
+                            dfs(grid, x, y)
+        return ans
 
 
 if __name__ == "__main__":
